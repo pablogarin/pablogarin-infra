13aa0b31a60b8667fed28eeb5b9b73c691f90cd0
Pablo Garin <pablo.garin@walmart.com>
2021-02-23 09:17:00 -0300

    Fix: path to work dir was static.

--- activate.sh
+++ activate.sh
@@ -1,4 +1,4 @@
 # Initialize an interactive session to a container with the gcp-cli with kubectl
 IMAGE_NAME=custom-gcp:latest
 docker build . -t $IMAGE_NAME
-docker run -it --rm -v /Users/pablo/kubernetes-manifest:/work -w /work --entrypoint /bin/bash $IMAGE_NAME
+docker run -it --rm -v $(pwd):/work -w /work --entrypoint /bin/bash $IMAGE_NAME
